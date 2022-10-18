279e8803bdcd8029f461880d43e5924526e831e4
BKB <blainekb94@gmail.com>
2022-10-17 22:42:49 -0500

    added in mutations

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const newHttpLink = createHttpLink({
-  uri: 'https://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
-  link: newHttpLink,
+  link: authLink.concat(newHttpLink),
   cache: new InMemoryCache(),
 });
 
@@ -18,14 +30,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>  
     </ApolloProvider>
   );
 }
