b7a7005fda7879d8fde614d91d7c59ffd9c46a5b
BKB <blainekb94@gmail.com>
2022-10-17 22:11:52 -0500

    modified resolvers and typeDefs toying with auth middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,54 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../util/auth');
+
+
 const resolvers = {
   Query: {
-    test: () => {
-      return 'test';
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password')
+        return userData;
+      }
+      throw new AuthenticationError('You are not logged in.');
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne( { email });
+      if (!user) {
+        throw new AuthenticationError('These credentials are incorrect.')
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const updateUser = await User.findOneAndUpdate(
+          { _id: context.user._id},
+          { $addToSet: {savedBooks: book} },
+          { new: true }
+        )
+        return updateUser;
+      }
+      throw new AuthenticationError('You must be logged in!')
+    },
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updateUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        )
+        return updateUser;
+      }
     }
   }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,36 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Query {
-    test: String
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: saveBookInput): User
+    deleteBook(bookId: String!): User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  } 
+  type Book {
+    bookId: ID
+    title: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
